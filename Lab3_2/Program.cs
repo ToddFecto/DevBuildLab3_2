148154514371a6791ee59f3fffc5d0e85cecb728
Todd Fecto <toddfecto@quickenloan.com>
2021-07-30 13:08:50 -0400

    Project updated through Quitting

--- Lab3_2/Program.cs
+++ Lab3_2/Program.cs
@@ -63,39 +63,38 @@ namespace Lab3_2
                     choice = Console.ReadLine().ToLower();
                 } while (choice != "a" && choice != "r" && choice != "c" && choice != "q");
 
+
                 if (choice == "a")
                 {
                     //Console.WriteLine("You entered A");
                     Console.WriteLine("\nYou chose to add a new menu item.");
 
                     string food = "";
+                    bool tryAgain = true;
                     bool menuCancel = true;
+                    while (tryAgain)
+                    {
+                        Console.Write("Enter the new menu item name (or type CANCEL to return to the main menu): ");
+                        food = Console.ReadLine();
+
+                        if (food == "CANCEL")       // Checks if the user entered CANCEL >>>go back to top menu and prompt for new choice
+                        {
+                            Console.WriteLine("You chose to Cancel this action and start again.");
+                            menuCancel = false;     // Changed so that it breaks out of the while {menuCancel loop below
+                            tryAgain = false;       // Changed so that it breaks out of the while {tryAgain loop
+                        }
+                        else if (items.ContainsKey(food))   // Checks if the item already exists in Dictionary
+                        {
+                            Console.WriteLine("Sorry that item already exists. Please try again.");
+                        }
+                        else
+                        {
+                            tryAgain = false;
+                        }
+                    }
+
                     while (menuCancel)
                     {
-                        bool tryAgain = true;
-
-                        while (tryAgain)
-                        {
-                            Console.Write("Enter the new menu item name (or type CANCEL to return to the main menu): ");
-                            food = Console.ReadLine();
-
-                            // Checks if the user entered CANCEL >>>go back to top menu and prompt for new choice
-                            if (food == "CANCEL")
-                            {
-                                Console.WriteLine("You chose to Cancel this action and start again.");
-                                menuCancel = false;
-                            }
-
-                            // Checks if the item already exists in Dictionary
-                            else if (items.ContainsKey(food))
-                            {
-                                Console.WriteLine("Sorry that item already exists. Please try again.");
-                            }
-                            else
-                            {
-                                tryAgain = false;
-                            }
-                        }
 
                         Console.Write("Enter the item price: $");
                         string price = Console.ReadLine();
@@ -107,6 +106,7 @@ namespace Lab3_2
                         foreach (var pair in items)
                         {
                             Console.WriteLine($"{pair.Key} ${pair.Value}");
+                            menuCancel = false;     // Changed so that it breaks out of the while {menuCancel loop
                         }
                     }
                 }
@@ -114,37 +114,90 @@ namespace Lab3_2
                 {
                     //Console.WriteLine("You entered R");
                     Console.WriteLine("\nYou chose to remove a menu item.");
-                    Console.Write("Enter the menu item to remove: ");
-                    string Remfood = Console.ReadLine();
-                    items.Remove(Remfood);
 
-                    Console.WriteLine("\nHere is our updated menu items and their prices:");
-                    foreach (var pair in items)
+                    string Remfood = "";
+                    bool tryAgain = true;
+                    bool menuCancel = true;
+                    while (tryAgain)
                     {
-                        Console.WriteLine($"{pair.Key} ${pair.Value}");
+                        Console.Write("Enter the menu item to remove (or type CANCEL to return to the main menu): ");
+                        Remfood = Console.ReadLine();
+
+                        if (Remfood == "CANCEL")       // Checks if the user entered CANCEL >>>go back to top menu and prompt for new choice
+                        {
+                            Console.WriteLine("You chose to Cancel this action and start again.");
+                            menuCancel = false;     // Changed so that it breaks out of the while {menuCancel loop below
+                            tryAgain = false;       // Changed so that it breaks out of the while {tryAgain loop
+                        }
+                        else if (!items.ContainsKey(Remfood))   // Checks if the item doesn't exist in Dictionary and repeats if not there
+                        {
+                            Console.WriteLine("Sorry that item doesn't exist. Please try again.");
+                        }
+                        else
+                        {
+                            tryAgain = false;
+                        }
                     }
-                }
-                else if (choice == "c")
-                {
-                    //Console.WriteLine("You entered C");
 
-                    Console.WriteLine("\nYou chose to change a menu item.");
-                    Console.Write("Enter the menu item to change: ");
-                    string chgFood = Console.ReadLine();
-
-                    if (items.ContainsKey(chgFood))   //Returns/passes the value of key menu item "chgFood" 's value
+                    while (menuCancel)
                     {
-                        Console.WriteLine($"This is the current price: ${items[chgFood]}");
-                        Console.Write("Enter the item's new price: $");
-                        string newPrice = Console.ReadLine();
-                        decimal itemNewPrice;
-                        decimal.TryParse(newPrice, out itemNewPrice);
-                        items[chgFood] = itemNewPrice;
+                        items.Remove(Remfood);      //Removes the item since action wasn't cancelled and it was determined to exist in dictionary
 
                         Console.WriteLine("\nHere is our updated menu items and their prices:");
                         foreach (var pair in items)
                         {
                             Console.WriteLine($"{pair.Key} ${pair.Value}");
+                            menuCancel = false;     // Changed so that it breaks out of the while {menuCancel loop
+                        }
+                    }
+                }
+                else if (choice == "c")
+                {
+                    //Console.WriteLine("You entered C");
+                    Console.WriteLine("\nYou chose to change a menu item.");
+
+                    string chgfood = "";
+                    bool tryAgain = true;
+                    bool menuCancel = true;
+                    while (tryAgain)
+                    {
+                        Console.Write("Enter the menu item to change (or type CANCEL to return to the main menu): ");
+                        chgfood = Console.ReadLine();
+
+                        if (chgfood == "CANCEL")       // Checks if the user entered CANCEL >>>go back to top menu and prompt for new choice
+                        {
+                            Console.WriteLine("You chose to Cancel this action and start again.");
+                            menuCancel = false;     // Changed so that it breaks out of the while {menuCancel loop below
+                            tryAgain = false;       // Changed so that it breaks out of the while {tryAgain loop
+                        }
+                        else if (!items.ContainsKey(chgfood))   // Checks if the item doesn't exist in Dictionary and repeats if not there
+                        {
+                            Console.WriteLine("Sorry that item doesn't exist. Please try again.");
+                        }
+                        else if (items.ContainsKey(chgfood))   //Returns/passes the value of key menu item "chgFood" 's value
+                        {
+                            Console.WriteLine($"This is the current price: ${items[chgfood]}");
+                            Console.Write("Enter the item's new price: $");
+                            string newPrice = Console.ReadLine();
+                            decimal itemNewPrice;
+                            decimal.TryParse(newPrice, out itemNewPrice);
+                            items[chgfood] = itemNewPrice;
+                            menuCancel = true;     // Changed so that it breaks out of the while {menuCancel loop below
+                            tryAgain = false;       // Changed so that it breaks out of the while {tryAgain loop
+                        }
+                        else
+                        {
+                            tryAgain = false;
+                        }
+                    }
+
+                    while (menuCancel)
+                    {
+                        Console.WriteLine("\nHere is our updated menu items and their prices:");
+                        foreach (var pair in items)
+                        {
+                            Console.WriteLine($"{pair.Key} ${pair.Value}");
+                            menuCancel = false;     // Changed so that it breaks out of the while {menuCancel loop
                         }
                     }
                 }
@@ -152,9 +205,9 @@ namespace Lab3_2
                 {
                     Console.WriteLine("Thank you for stopping by!!");
                     keepGoing = false;
-
                 }
             }
         }
     }
 }
+
